a4a2b363702fb51b6d6ee17a656b5ac69cd596d1
surender <surenderkiran00@gmail.com>
2018-06-12 14:23:25 +0530

    git successfull creation

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
+#include "include.h"
 
 int main()
 
 {
-	printf("welcome to linux\n");
+	int x,y;
+	x=10,y=20;
+
+	printf("welcome to c code\n");
+	
+	printf("%d\n",add(x,y));
+	printf("%d\n",sub(x,y));
+
 return 0;
 }
